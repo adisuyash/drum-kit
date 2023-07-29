dfcccc4dd08216397c7e03a608fdc537b88044d9
adisuyash <116362593+adisuyash@users.noreply.github.com>
2023-07-29 23:19:24 +0530

    initial commit

---
+++ README.md
@@ -0,0 +1,16 @@
+# drum-kit
+live: https://adisuyash.github.io/drum-kit/
+
+Use keyboard keys to produce pleasant music 🎷
+
+What I learned :
+- adding event listeners
+    - `addEventListener('type',function)`
+- how to use mp3 in JavaScript & play it on key press
+    - <code>let musicName = new Audio('audioFile-path');
+    <br>
+    ‎ musicName.play();</code>
+    
+- add and remove HTML classes using JavaScript (also, pre-define CSS for new className)
+    - `document.querySelector('selectorName').classList.add('className')`
+    - `document.querySelector('selectorName').classList.remove('className')`
---
+++ script.js
@@ -0,0 +1,69 @@
+// Determining Button press
+let numberOfDrums = document.querySelectorAll(".drums").length;
+for (let i = 0; i < numberOfDrums; i++) {
+    document.querySelectorAll(".drums")[i].addEventListener("click", function () {
+        let buttonInnerHTML = this.innerHTML; // it stores 'w a s d j k l' , which is clicked 
+        makeSound(buttonInnerHTML);
+        buttonAnimation(buttonInnerHTML);
+    });
+
+}
+
+// Determining Keyboard press
+document.addEventListener("keypress", function (event) {
+
+    // console.log(event) - // to display Keypress Event and then accessing the keypress by event.key 
+
+    makeSound(event.key);
+    buttonAnimation(event.key);
+})
+
+function makeSound(key) {
+    switch (key) {
+        case "w":
+            let crash = new Audio("sounds/crash.mp3")
+            crash.play();
+            break;
+        case "a":
+            let tom1 = new Audio("sounds/tom-1.mp3")
+            tom1.play();
+            break;
+        case "s":
+            let tom2 = new Audio("sounds/tom-2.mp3")
+            tom2.play();
+            break;
+        case "d":
+            let kickBass = new Audio("sounds/kick-bass.mp3")
+            kickBass.play();
+            break;
+        case "j":
+            let tom3 = new Audio("sounds/tom-3.mp3")
+            tom3.play();
+            break;
+        case "k":
+            let tom4 = new Audio("sounds/tom-4.mp3")
+            tom4.play();
+            break;
+        case "l":
+            let snare = new Audio("sounds/snare.mp3")
+            snare.play();
+            break;
+
+        default:
+            break;
+    }
+}
+
+// Adding Animation on Keyboard Presses & Mouse Clicks
+function buttonAnimation(key) {
+    let keyAnimation = document.querySelector("." + key);
+
+    // "pressed" Class is added to buttons when they are pressed
+    keyAnimation.classList.add("pressed");
+
+    // "pressed" Class is removed after 1s
+    setTimeout(function () {
+        keyAnimation.classList.remove("pressed");
+    }, 100)
+
+}
